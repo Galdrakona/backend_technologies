e3abaaf72aeacfb82f37fff4ebd3c7feb1b0f9c2
Galdrakona <e.pigeon.drake@gmail.com>
2024-11-17 20:47:23 +0100

    new branch hello-world import

--- hollymovies/urls.py
+++ hollymovies/urls.py
@@ -17,6 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from viewer.views import hello, hello2, hello3, add, add2, hello4
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('hello/', hello),
+    path('hello2/<str:word>/', hello2),
+    path('hello3', hello3),
+    path('hello4', hello4),
+    path('add/<int:num1>/<int:num2>', add),
+    path('add2', add2),
 ]
--- viewer/views.py
+++ viewer/views.py
@@ -1,3 +1,45 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def hello(request):
+    # http://127.0.0.1:8000/hello/
+    return HttpResponse('Hello, World!')
+
+
+def hello2(request, word):
+    # http://127.0.0.1:8000/hello2/nice/
+    return HttpResponse(f'Hello, {word} world!')
+
+
+def hello3(request):
+    # http://127.0.0.1:8000/hello3?world=cruel
+    word = request.GET.get('word', '')
+    return HttpResponse(f'Hello, {word} world!')
+
+
+def hello4(request):
+    word = request.GET.get('word', '')
+    context = {'word': word}
+    return render(request=request, template_name="hello.html", context=context)
+
+
+def add(request, num1, num2):
+    # http://127.0.0.1:8000/add/2/3/
+    return HttpResponse(f"{num1} + {num2} = {num1 + num2}")
+
+
+def add2(request):
+    # http://127.0.0.1:8000/add2 -> 0 + 0 = 0
+    # http://127.0.0.1:8000/add2?num1=2 -> 2 + 0 = 2
+    # http://127.0.0.1:8000/add2?num1=2&num2=3 -> 2 + 3 = 5
+    # http://127.0.0.1:8000/add2?num2=3&num1=2 -> 2 + 3 = 5
+    num1 = int(request.GET.get('num1', 0))
+    num2 = int(request.GET.get('num2', 0))
+    return HttpResponse(f"{num1} + {num2} = {num1 + num2}")
+
+
+#TODO add3 s template
+def add3(request):
+    # http://127.0.0.1:8000/add3?num1=5&num2=6
+    pass
