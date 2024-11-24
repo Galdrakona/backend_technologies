cbf88c8c06c2f644e5a737980e678f6ac65553fa
Galdrakona <e.pigeon.drake@gmail.com>
2024-11-24 16:21:02 +0100

    movie details

--- viewer/views.py
+++ viewer/views.py
@@ -7,8 +7,17 @@ from viewer.models import Movie
 def home(request):
     return render(request, "home.html",)
 
+
 def movies(request):
     # movies_list = Movie.objects.all()
     # context = {'movies': movies_list}
     # return render(request, "movies.html", context)
     return render(request, "movies.html", {'movies': Movie.objects.all()})
+
+
+def movie(request, pk):
+    if Movie.objects.filter(id=pk).exists():
+        movie_ = Movie.objects.get(id=pk)
+        context = {'movie': movie_}
+        return render(request, "movie.html", context)
+    return movies(request)
